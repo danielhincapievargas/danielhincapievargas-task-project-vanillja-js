884867360f8ea92f5927861584c6aaa88ab4b088
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-06-21 23:29:21 -0500

    Creación de la función handleSubmit

--- js/index.js
+++ js/index.js
@@ -2,3 +2,56 @@ const input = document.querySelector('.input-tarea');
 const addButton = document.querySelector('.add-button');
 const taskList = document.querySelector('.task-list');
 const mainTaskContainer = document.querySelector('.main-task-container');
+const mainContainer = document.querySelector('main');
+
+let id = 1;
+
+function handleSubmit(event) {
+    event.preventDefault();
+    if(input.value !== ''){
+
+    const onlyTaskContainer = document.createElement('div');
+    onlyTaskContainer.classList.add('only-tasks');
+
+    const taskContainer = document.createElement('div');
+    taskContainer.classList.add('task-container');
+
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('name', `tarea${id}`);
+    checkbox.setAttribute('type', 'checkbox');
+
+    const label = document.createElement('label');
+    label.setAttribute('for', `tarea${id}`);
+    label.innerText = input.value;
+    input.value = '';
+
+    document.createElement('hr');
+
+    id = id + 1;
+
+    mainTaskContainer.appendChild(onlyTaskContainer);
+    onlyTaskContainer.appendChild(taskContainer)
+    taskContainer.appendChild(checkbox);
+    taskContainer.appendChild(label);
+
+    
+if(id === 2){
+        const deleteButton = document.createElement('div');
+        deleteButton.classList.add('delete-button');
+
+        const buttonDelete = document.createElement('button');
+        buttonDelete.innerHTML = 'Delete all done';
+
+        mainContainer.appendChild(deleteButton);
+        deleteButton.appendChild(buttonDelete);
+
+    }
+    
+
+    }
+
+
+}
+
+addButton.addEventListener('click', handleSubmit)
