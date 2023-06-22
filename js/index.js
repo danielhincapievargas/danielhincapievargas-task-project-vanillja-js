a4879ef2e0014ba22899a857366341616d7cb854
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-06-21 21:20:54 -0500

    estructura html con estilos css

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const input = document.querySelector('.input-tarea');
+const addButton = document.querySelector('.add-button');
+const taskList = document.querySelector('.task-list');
+const mainTaskContainer = document.querySelector('.main-task-container');
